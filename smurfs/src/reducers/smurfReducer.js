84b525e1c76789f86c308f185d7f8ff747f67a5d
Mike Sinnema <msinnema33@gmail.com>
2020-02-21 09:33:10 -0700

    updated reducer

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,32 @@
+import { FETCH_DATA, UPDATE_SMURFS, SET_ERROR } from "../actions";
+
+const initialState = {
+  smurfs: [],
+  isFetchingData: false,
+  error: ""
+};
+
+export const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_DATA:
+      return {
+        ...state,
+        isFetchingData: true,
+        smurfs: []
+      };
+    case UPDATE_SMURFS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        isFetchingData: false
+      };
+    case SET_ERROR:
+      return {
+        ...state,
+        isFetchingData: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
