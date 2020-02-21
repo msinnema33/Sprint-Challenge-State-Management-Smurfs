6c5d3c4c6bdf06dfbe23eedaa12d4820d5c6e385
Mike Sinnema <msinnema33@gmail.com>
2020-02-21 11:17:05 -0700

    working on form

--- smurfs/src/components/AddSmurf.js
+++ smurfs/src/components/AddSmurf.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm  } from 'react-hook-form';
 import { connect } from "react-redux";
 import { postData } from "../actions";
 import Axios from 'axios';
 
 const AddSmurf = props => {
+    const [newSmurf, setNewSmurf] = useState({
+        name: '',
+        age: '',
+        height: ''
+      });
+
+    const handleChange = e => {
+        setNewSmurf({...newSmurf, [e.target.name]: e.target.value});
+    };  
    const {register, handleSubmit, error } = useForm();
+   
     const handlePostData = e => {
         // Axios
         // .post("http://localhost:3333/smurfs")
@@ -28,22 +38,28 @@ const AddSmurf = props => {
                     <h2>Add Smurf data</h2>
                     <input
                         type='text'
-                        name='name'
+                        // name='name'
                         placeholder='Enter a name.... '
+                        value={newSmurf.name}
+                        onChange={handleChange}
                         ref={register({ required: true, maxLength: 15})}
                     />
                     {error.name && <p>Name is required</p>}
                     <input
                         type='number'
-                        name='age'
+                        // name='age'
                         placeholder='Enter age...'
+                        value={newSmurf.age}
+                        onChange={handleChange}
                         ref={register({ required: true, maxLength: 3})}
                     />
                     {error.age && <p>Age is required</p>}
                     <input
                         type='text'
-                        name='height'
+                        // name='height'
                         placeholder='Add height +cm'
+                        value={newSmurf.height}
+                        onChange={handleChange}
                         ref={register({ required: true, maxLength: 10})}
                     />
                     {error.height && <p>Height is required</p>}    
