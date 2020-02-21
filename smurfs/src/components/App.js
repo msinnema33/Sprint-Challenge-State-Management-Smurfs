dcb47bbfb31e721f9cd72562eeddaa560ebc1ed4
Mike Sinnema <msinnema33@gmail.com>
2020-02-21 09:42:37 -0700

    updated App

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from "react";
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
+import SmurfForm from './SmurfForm';
+import SmurfList from './SmurfList';
+import { createStore, applyMiddleware } from 'redux';
+import { smurfReducer as reducer } from '../reducers/smurfReducer';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+export default function App() {
+  return (
+    <Provider store={store}>
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
+        <h1>SMURFS! 2.0 W/ Redux by Mike Sinnema</h1>
+        <div>Welcome to the state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <SmurfForm />
+        <SmurfList />
       </div>
-    );
-  }
+    </Provider>
+  );
 }
 
-export default App;
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>SMURFS! 2.0 W/ Redux</h1>
+//         <div>Welcome to your state management version of Smurfs!</div>
+//         <div>Start inside of your `src/index.js` file!</div>
+//         <div>Have fun!</div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
